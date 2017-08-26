9e868d5d5b3f2a75b30b046d6b10fe9b5ec6fccb
aming183 <aming.lai@gmail.com>
2017-08-26 17:33:41 +0800

    練習建立 /charts/flot 子路由頁面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,18 @@ import { Page2Component } from './page2/page2.component';
 import { fallbackRoute } from './shares/fallback-Route';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { FlotComponent } from './charts/flot/flot.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'cards', component: CardsComponent },
+  { path: 'charts',
+    children: [
+      { path: '', redirectTo: 'flot', pathMatch: 'full' },
+      { path: 'flot', component: FlotComponent}
+    ]
+  },
   fallbackRoute
 ];
 
