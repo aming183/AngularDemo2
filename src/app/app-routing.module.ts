56286c62d949dd8fdce9f56468166fe32ea07651
aming183 <aming.lai@gmail.com>
2017-08-26 14:28:27 +0800

    HashLocationStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
-import { Page2Component  } from './page2/page2.component';
+import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-  { path: 'page1', component: Page1Component},
-  { path: 'page2', component: Page2Component}
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    { enableTracing: true, useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
