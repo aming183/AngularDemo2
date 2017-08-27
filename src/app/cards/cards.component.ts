8aa85c974ce120fc720ae029d912733b06a51a4e
aming183 <aming.lai@gmail.com>
2017-08-27 10:06:04 +0800

    嘗試解決在相同路由之間切換不同參數值所造成的問題

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -14,8 +14,26 @@ export class CardsComponent implements OnInit {
     , private route: ActivatedRoute) { }
 
   ngOnInit() {
+    /*
     this.type = this.route.snapshot.params['type'];
     console.log(this.type);
+    */
+
+    this.route.params.subscribe(params => {
+      this.type = params['type'];
+    });
+
+
+  }
+
+  doTypePlus1(type) {
+    var num = +type;
+    this.router.navigateByUrl('/cards/' + (num + 1));
+  }
+
+  doTypeMinus1(type) {
+    var num = +type;
+    this.router.navigateByUrl('/cards/' + (num - 1));
   }
 
 }
