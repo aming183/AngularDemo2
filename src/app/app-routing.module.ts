c2000de879d7ed3a3c4f65ee93988ab8ad45b921
aming183 <aming.lai@gmail.com>
2017-08-27 11:31:05 +0800

    建立 Charts 子路由模組與實現延遲載入

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,15 @@ import { Page2Component } from './page2/page2.component';
 import { fallbackRoute } from './shares/fallback-Route';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
-import { FlotComponent } from './charts/flot/flot.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'cards', component: CardsComponent },
   { path: 'cards/:type', component: CardsComponent },
-  { path: 'charts',
-    children: [
-      { path: '', redirectTo: 'flot', pathMatch: 'full' },
-      { path: 'flot', component: FlotComponent}
-    ]
+  {
+    path: 'charts',
+    loadChildren: './charts/charts.module#ChartsModule'
   },
   fallbackRoute
 ];
